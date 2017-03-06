681210769926ee842e94beb9c64b12eb31d88fe3
Yihua Zhang <michaelzhang2@msn.com>
2017-03-06 13:41:00 -0500

    pushing up route builds

---
+++ src/components/adlibs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <h2>The Ad Lib Test</h2>
+        <p>What makes a rapper great? There's a monstrous flow, mind boggling wordplay, vivid storytelling and ear-wormingly recognizable ad libs.</p>
+      </div>
+    )
+  }
+})
--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router';
 
-export default () => {
-  return (
-    <div>
-      Rapppps
-    </div>
-  )
-}
+export default React.createClass({
+  render() {
+    return (
+      <div className="home-container">
+        <svg width="100" height="100">
+          <defs>
+            <pattern id="adlibs-image" patternUnits="userSpaceOnUse" width="100" height="100">
+              <image xlinkHref="/assets/adlibs.jpg" x="0" y="0" width="100" height="100" />
+            </pattern>
+          </defs>
+          <polygon points="0,0 100,0 50,100" fill="url(#a)" />
+        </svg>
+        <div className="triangle-option container-even">
+          <Link to="/adlibs" />
+        </div>
+
+        <div className="triangle-option container-odd">
+          <Link to="/rappers" />
+        </div>
+      </div>
+    )
+  }}
+)
